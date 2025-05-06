b4a2ee17b25ff0681c2a70e973c2517e14d60efc
albert <albertlyuan@gmail.com>
2025-05-06 23:44:35 +0000

    added test syntax

--- macros/tests/expect_column_values_to_match_regex_list_regexp_like.sql
+++ macros/tests/expect_column_values_to_match_regex_list_regexp_like.sql
@@ -1,3 +1,5 @@
+{% test expect_column_values_to_match_regex_list_regexp_like(model, column_name, regex_list) %}
+
 {% set column_name = kwargs.get('column') %}
 {% set regex_list = kwargs.get('regex_list') %}
 {% set row_condition = kwargs.get('row_condition', 'true') %}
@@ -15,3 +17,4 @@ with validation_errors as (
 select *
 from validation_errors
 
+{% endtest %}
