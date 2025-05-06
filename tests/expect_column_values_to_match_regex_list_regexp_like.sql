c59a29e5949e0884221caa2133179858fcd8c84e
albert <albertlyuan@gmail.com>
2025-05-06 20:36:38 +0000

    custom macro to replace regex_list

---
+++ tests/expect_column_values_to_match_regex_list_regexp_like.sql
@@ -0,0 +1,19 @@
+-- tests/expect_column_values_to_match_regex_list_regexp_like.sql
+
+{% set column_name = kwargs.get('column') %}
+{% set regex_list = kwargs.get('regex_list') %}
+{% set row_condition = kwargs.get('row_condition', 'true') %}
+with validation_errors as (
+    select *
+    from {{ model }}
+    where {{ row_condition }}
+    and not (
+        {% for pattern in regex_list %}
+            REGEXP_LIKE({{ column_name }}, '{{ pattern }}') {% if not loop.last %} OR {% endif %}
+        {% endfor %}
+    )
+)
+
+select *
+from validation_errors
+
